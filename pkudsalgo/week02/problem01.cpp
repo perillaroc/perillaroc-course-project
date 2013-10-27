3252a9386cb8a667e0ad70eae5b4f826d1fa34d5
perillaroc <perillaroc@gmail.com>
2013-10-27 23:01:13 +0800

    add new output

--- pkudsalgo/week02/problem01.cpp
+++ pkudsalgo/week02/problem01.cpp
@@ -1,55 +1,60 @@
 #include <iostream>
-#include <vector>
+#include <cstdlib>
+
 using namespace std;
 
-template<typename T> struct MyArrayQueue
-{
-	MyArrayQueue(){
-		max_size_=102;
-		inner_vector_.resize(max_size_);
-		front=1;
-		rear=0;
-	}
-
-	void clear()
-	{
-		front=rear+1;
-	}
-	
-	bool enQueue(const T &item){
-		if((rear+2)%max_size_ == front)
+template <class T> 
+class arrQueue{ 
+private: 	
+	int 	mSize; 						// 存放队列的数组的大小
+	int 	front;							// 表示队头所在位置的下标
+	int 	rear;							// 表示队尾所在位置的下标
+	T 	    *qu;							// 存放类型为T的队列元素的数组
+public: 								// 队列的运算集
+   	arrQueue(int size=120)  {    				// 创建队列的实例
+		mSize = size +1;					// 浪费一个存储空间，以区别队列空和队列满
+		qu = new T[mSize];
+		front = rear = 0;
+}
+   	~arrQueue()  {  					// 消除该实例，并释放其空间
+		delete [] qu;
+}
+void clear() {						// 清空队列
+	front = rear; 
+}
+   	bool enQueue(const T item)  { 			//  item入队，插入队尾
+		if (((rear + 1 ) % mSize) == front) {
 			return false;
-		rear = (rear+1)%max_size_;
-		inner_vector_[rear]=item;
+		}
+		qu[rear] = item;
+		rear = (rear +1) % mSize;  			// 循环后继
 		return true;
 	}
-	bool deQueue(T &item)
-	{
-		if(length()==0)
+ 	bool deQueue(T*item)  {     			// 返回队头元素并从队列中删除
+		if ( front == rear)  {
 			return false;
-		item=inner_vector_[front];
-		front = (front+1)%max_size_;
+		}
+		*item = qu[front];
+		front = (front +1) % mSize;
+		return true;
+ 	}
+    bool getFront(T* item) {	     			// 返回队头元素，但不删除
+		if (front == rear)  {
+			return false;
+		}
+		*item = qu[front];
 		return true;
 	}
-
-	int length()
-	{
-		return (max_size_+rear-front+1)%max_size_;
+	void print() {	     			// 返回队头元素，但不删除
+		if (front == rear)  {
+		}
+		int p = front;
+		while(p != rear ) {
+              cout << qu[p] << " ";
+              p =( p + 1)  % mSize;  
+        } 
+        cout << endl;
 	}
-
-	bool frontItem(T &item)
-	{
-		if(length()==0)
-			return false;
-		item=inner_vector_[front];
-		return true;
-	}
-
-private:
-	vector<T> inner_vector_;
-	int max_size_;
-	int front;
-	int rear;
 };
 
 struct Card
@@ -74,7 +79,7 @@ struct Card
 
 int main()
 {
-	vector< MyArrayQueue<Card> > queues(9);
+	arrQueue<Card> queues[9];
 	int n;
 	cin>>n;
 	for(int i=0;i<n;i++)
@@ -83,14 +88,14 @@ int main()
 		cin>>c>>p;
 		Card a_card(c,p);
 		queues[p-1].enQueue(a_card);
-	}
-	vector< MyArrayQueue<Card> > color_queues(4);
+	}	
+	arrQueue<Card> color_queues[4];
 	for(int i=0;i<9;i++)
 	{
 		cout<<"Queue"<<i+1<<":";
 		Card a_card;
 		int j=0;
-		while(queues[i].deQueue(a_card))
+		while(queues[i].deQueue(&a_card))
 		{
 			int x=static_cast<int>(a_card.color-'A');
 			color_queues[x].enQueue(a_card);
@@ -101,19 +106,30 @@ int main()
 		}
 		cout<<endl;
 	}
-	int j=0;
 	for(int i=0;i<4;i++)
 	{
+		int j=0;
 		Card a_card;
-		while(color_queues[i].deQueue(a_card))
+		cout<<"Queue";
+		cout<<char('A'+i)<<":";
+		while(color_queues[i].deQueue(&a_card))
 		{
 			if(j!=0)
 				cout<<" ";
 			a_card.print();
 			j++;
+			queues[0].enQueue(a_card);
 		}
+		cout<<endl;
+	}
+	Card a_card;
+	if(queues[0].deQueue(&a_card))
+		a_card.print();
+	while(queues[0].deQueue(&a_card))
+	{
+		cout<<" ";
+		a_card.print();
 	}
 	cout<<endl;
-	
 	return 0;
 }
