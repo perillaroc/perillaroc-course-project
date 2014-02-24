21daaf0507e11e5cc948e0d1dd75a137719944ac
perillaroc <perillaroc@gmail.com>
2014-02-25 00:10:53 +0800

    create problem01

--- pkudsalgo/week06/problem01.cpp
+++ pkudsalgo/week06/problem01.cpp
@@ -1,9 +1,143 @@
 #include <iostream>
 #include <string>
+#include <stack>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
+class TreeNode
+{
+public:
+    TreeNode():
+        first_child(NULL),
+        next_sibling(NULL)
+    {}
+    string value;
+    TreeNode *first_child;
+    TreeNode *next_sibling;
+};
+
+class Tree
+{
+public:
+    Tree():root(NULL){}
+    
+    void preOrderVisit()
+    {
+        stack<TreeNode*> s;
+        if(root==NULL)
+            return;
+        s.push(root);
+        while(!s.empty())
+        {
+            TreeNode* p = s.top();
+            cout<<p->value<<endl;
+            s.pop();
+            if(p->next_sibling)
+            {
+                s.push(p->next_sibling);
+            }
+            if(p->first_child)
+            {
+                s.push(p->first_child);
+            }
+        }
+    }
+    
+    TreeNode* find()
+    {
+        stack<TreeNode*> s;
+        if(root==NULL)
+            return;
+        s.push(root);
+        while(!s.empty())
+        {
+            TreeNode* p = s.top();
+            cout<<p->value<<endl;
+            s.pop();
+            if(p->next_sibling)
+            {
+                s.push(p->next_sibling);
+            }
+            if(p->first_child)
+            {
+                s.push(p->first_child);
+            }
+        }
+    }
+    TreeNode* root;
+};
+
+void doOneTest()
+{
+    stack<TreeNode*> s;
+    const int buffer_size = 50;
+    char buffer[buffer_size];
+    Tree tree;
+    tree.root = new TreeNode;
+    TreeNode* p1 = tree.root;
+    string str;
+    getline(cin, str);
+    while(!str.empty())
+    {
+        if(str=="{")
+        {
+            s.push(p1);
+            getline(cin, str);
+            TreeNode* p2 = new TreeNode;
+            p2->value = str;
+            p1->first_child = p2;
+            p1 = p2;
+        }
+        else if(str=="}")
+        {
+            p1 = s.top();
+            s.pop();
+        }
+        else
+        {
+            TreeNode* p2 = new TreeNode;
+            p2->value = str;
+            p1->next_sibling = p2;
+            p1 = p2;
+        }
+        cout<<str<<endl;
+        getline(cin, str);
+    }
+    
+    getline(cin, str);
+    string value_to_find = str;
+
+    getline(cin,str);
+    if(!str.empty())
+    {
+       cerr<<"ERROR"<<endl;
+       return;
+    }
+    getline(cin,str);
+    if(!str.empty())
+    {
+       cerr<<"ERROR"<<endl;
+       return;
+    }
+
+    tree.preOrderVisit();
+
+}
+
+
 int main()
 {
+    string str;
+    getline(cin, str);
+    stringstream ss(str);
+    int count;
+    ss>>count;
+    for(int i=0;i<count;i++)
+    {
+        doOneTest();
+    }
+    
     return 0;
 }
