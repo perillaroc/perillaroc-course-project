0967d23e9fdd554c0557b583bd7f433dd008d3c7
perillaroc <perillaroc@gmail.com>
2014-04-24 21:52:01 +0800

    week03 quiz 04

--- getdata-002/week03/quiz.R
+++ getdata-002/week03/quiz.R
@@ -3,3 +3,16 @@ question01<-function(){
   agricultureLogical <-(q1$AGS==6 & q1$ACR==3 & !is.na(q1$AGS) & !is.na(q1$ACR))
   which(agricultureLogical)
 }
+
+question02<-function(){
+  q2<- readJPEG("question02.jpg",native=TRUE)
+  quantile(q2,probs=c(0.3,0.8))
+}
+
+question04<-function(){
+  q4_gdp<-read.csv("question03-GDP.csv",header=TRUE)
+  q4_edu<-read.csv("question03-Country.csv",header=TRUE)
+  q4<-merge(q4_gdp,q4_edu,by.x="X",by.y="CountryCode",all=TRUE)
+  q4_nona<- q4[!is.na(q4$Gross.domestic.product.2012),]
+  tapply(q4_nona$Gross.domestic.product.2012, q4_nona$Income.Group,mean)
+}
